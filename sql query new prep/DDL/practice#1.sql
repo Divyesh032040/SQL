d14aa4aa3c4a0a1902ebcffb3d146515073a5d29
Divyesh Parmar <dp032040@gmail.com>
2025-04-05 19:05:24 +0530

    order by and group by

--- sql query new prep/DDL/practice#1.sql
+++ sql query new prep/DDL/practice#1.sql
@@ -1,5 +1,6 @@
 
-
+-- intermediate query
+-- PRINT EMPLOYEES TABLE
 SELECT * FROM employees;
 
 -- Show all 'Managers' working in the 'HR' department.
@@ -17,4 +18,26 @@ SELECT * FROM EMPLOYEES WHERE MANAGER IS NULL;
 
 -- List all unique designations.
 SELECT DISTINCT designation FROM employees;
+
+
+
+-- HARD LEVEL QUERIES
+
+-- 🔴 Show the total number of employees in each department.
+    SELECT department , COUNT(*) AS total_emp FROM employees GROUP_BY department;
+
+-- 🔴 Show the average salary by department.
+    SELECT SALARY , AVG(SALARY) AS AVG_SALARY FROM EMPLOYEES GROUP BY SALARY;
+
+-- 🔴 Show the highest paid employee in the company.
+    SELECT * from employees order by salary desc limit 1;
+
+-- 🔴 Show the 2nd highest salary employee.
+    SELECT * FROM EMPLOYEES ORDER BY SALARY DESC LIMIT 1 OFFSET 1;
+
+-- 🔴 Show employees sorted by hire date (most recent first).
+    SELECT * FROM EMPLOYEES ORDER BY HIRED_ON DESC;
+
+-- 🔴 Show employees whose commission is greater than 10% of their salary.
+    SELECT * FROM EMPLOYEES WHERE COMMISSION > 0.1 * SALARY;
 
